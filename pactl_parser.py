c9bb866128517d858232781c661033f99e6a5e84
fm16191 <56755856+fm16191@users.noreply.github.com>
2022-11-30 03:21:20 +0100

    Some kind of "optimizations"

--- pactl_parser.py
+++ pactl_parser.py
@@ -1,22 +1,14 @@
-# import subprocess
 # import sys
 import json
 import os
-
-def get_indent(line): # Returns clean line & its indentation
-    indent = 0
-    while(line.startswith("\t")):
-        line = line[1:]
-        indent = indent + 1
-    return indent, line
-
-# Using subprocess
-# sys.stdout.reconfigure(encoding='utf-8')
-# result = subprocess.check_output("pactl list sink-inputs", shell=True)
-# result = result.decode("utf-8")
+import subprocess
 
 # Using popen
-result = os.popen("pactl list sink-inputs").read()
+# result = os.popen("pactl list sink-inputs").read()
+
+# Using subprocess (much faster)
+p = subprocess.run("pactl list sink-inputs".split(), capture_output=True)
+result = p.stdout.decode()
 
 # Syntax fix
 result = result.replace(u"\xa0", " ") # Broken encoding for spaces
@@ -27,7 +19,9 @@ sinks = dict()
 
 for oline in result.split("\n"):
     if oline == "": continue # Skip empty lines
-    (indent, line) = get_indent(oline) # Indentation indicates the JSON structure
+    # Indentation indicates the JSON structure
+    indent = oline.count('\t')
+    line = oline[indent:]
     if indent == 0: # Get sink name
         sink_name = line.split("#")[-1]
         sinks[sink_name] = {}
@@ -35,14 +29,16 @@ for oline in result.split("\n"):
         if line.startswith("        "): # Output is over two lines
             sinks[sink_name][name] = sinks[sink_name][name] + " " + line.strip()
         else:
-            name = line[:line.index(":")].strip()
-            value = line[line.index(":")+1:].strip()
+            ii = line.index(":")
+            name = line[:ii].strip()
+            value = line[ii + 1:].strip()
             sinks[sink_name][name] = value
     elif indent == 2: # Get sink object properties
         if sinks[sink_name][name] == "":
             sinks[sink_name][name] = {}
-        sub_name = line[:line.index("=")].strip()
-        sub_value = line[line.index("=")+1:].strip()
+        ii = line.index("=")
+        sub_name = line[:ii].strip()
+        sub_value = line[ii + 1:].strip()
         if sub_value[0] == "'" and sub_value[-1] == "'":
             sub_value = sub_value[1:-1]
         sinks[sink_name][name][sub_name] = sub_value
